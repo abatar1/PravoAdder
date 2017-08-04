42f4d504eeab04ef97ae4edd22642149d63b05eb
abatar1 <evgenyhalzov>
2017-08-04 11:21:45 +0500

    console stuff

--- PravoAdder/ConsoleHelper.cs
+++ PravoAdder/ConsoleHelper.cs
@@ -1,12 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PravoAdder.DatabaseEnviroment;
+using PravoAdder.Domain;
+using PravoAdder.Reader;
 
 namespace PravoAdder
 {
-    class ConsoleHelper
+    public class ConsoleHelper
     {
+        public static void Autentification(Settings settings, DatabaseFiller filler)
+        {
+            while (true)
+            {
+                if (settings.Login == string.Empty)
+                {
+                    Console.WriteLine("Login: ");
+                    settings.Login = Console.ReadLine();
+                }
+
+                Console.WriteLine("Password: ");
+                settings.Password = Console.ReadLine();
+
+                Console.WriteLine($"Login as {settings.Login}...");
+                try
+                {
+                    var authenticationStatus = filler.Authentication(
+                        login: settings.Login,
+                        password: settings.Password);
+                    if (authenticationStatus) break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public static void LoadConfigFromConsole(Settings settings)
+        {
+            if (settings.FolderName == string.Empty)
+            {
+                Console.WriteLine("Folder name: ");
+                settings.FolderName = Console.ReadLine();
+            }
+
+            if (settings.ProjectTypeName == string.Empty)
+            {
+                Console.WriteLine("Project type name: ");
+                settings.ProjectTypeName = Console.ReadLine();
+            }
+        }
+
+        public static IEnumerable<IDictionary<int, string>> ReadExcelFile()
+        {
+            string excelFilename;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Write excel filename: ");
+                    excelFilename = $"{Console.ReadLine()}.xlsx";
+                    break;
+                }
+                catch (FileNotFoundException ex)
+                {
+                    Console.WriteLine($"{ex.Message}");
+                }
+            }
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Reading excel file...");
+                    return ExcelReader.ReadDataFromTable(excelFilename).ToList();
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine($"{ex.Message} Please close file and press Enter.");
+                    Console.ReadLine();
+                }
+            }
+        }
     }
 }
--- PravoAdder/Program.cs
+++ PravoAdder/Program.cs
@@ -9,93 +9,18 @@ using PravoAdder.Reader;
 namespace PravoAdder
 {
     internal class Program
-    {
-        public static void Autentification(Settings settings, DatabaseFiller filler)
-        {
-            while (true)
-            {
-                if (settings.Login == string.Empty)
-                {
-                    Console.WriteLine("Login: ");
-                    settings.Login = Console.ReadLine();
-                }
-
-                Console.WriteLine("Password: ");
-                settings.Password = Console.ReadLine();
-
-                Console.WriteLine($"Login as {settings.Login}...");
-                try
-                {
-                    var authenticationStatus = filler.Authentication(
-                        login: settings.Login,
-                        password: settings.Password);
-                    if (authenticationStatus) break;
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }                
-            }           
-        }
-
-        public static void LoadConfigFromConsole(Settings settings)
-        {
-            if (settings.FolderName == string.Empty)
-            {
-                Console.WriteLine("Folder name: ");
-                settings.FolderName = Console.ReadLine();
-            }
-
-            if (settings.ProjectTypeName == string.Empty)
-            {
-                Console.WriteLine("Project type name: ");
-                settings.ProjectTypeName = Console.ReadLine();
-            }
-        }
-
-        public static IEnumerable<IDictionary<int, string>> ReadExcelFile()
-        {
-            string excelFilename;
-            while (true)
-            {
-                try
-                {
-                    Console.WriteLine("Write excel filename: ");
-                    excelFilename = $"{Console.ReadLine()}.xlsx";
-                    break;
-                }
-                catch (FileNotFoundException ex)
-                {
-                    Console.WriteLine($"{ex.Message}");
-                }
-            }
-
-            while (true)
-            {
-                try
-                {
-                    Console.WriteLine("Reading excel file...");
-                    return ExcelReader.ReadDataFromTable(excelFilename).ToList();
-                }
-                catch (IOException ex)
-                {
-                    Console.WriteLine($"{ex.Message} Please close file and press Enter.");
-                    Console.ReadLine();
-                }
-            }         
-        }
-
+    {      
         private static void Main(string[] args)
         {
-            var excel = ReadExcelFile();
+            var excel = ConsoleHelper.ReadExcelFile();
 
             Console.WriteLine("Reading config files...");
             var blocksInfo = BlockReader.ReadBlocks("blocksInfo.json").ToList();
             var settings = SettingsReader.Read("config.json");
 
             var filler = new DatabaseFiller();
-            Autentification(settings, filler);
-            LoadConfigFromConsole(settings);
+            ConsoleHelper.Autentification(settings, filler);
+            ConsoleHelper.LoadConfigFromConsole(settings);
             settings.Save("config.json");
 
             foreach (var excelRow in excel)
